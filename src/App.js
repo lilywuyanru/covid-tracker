c777d2d56448e3f46b6de33f2a426b5f4e5d9992
Lily <lilywuyanru@gmail.com>
2020-04-09 03:18:19 -0400

    Add nav bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Navigation } from './components/navigation/navigation.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Covid Tracker
-        </a>
-      </header>
+        <Navigation />   
     </div>
   );
 }
