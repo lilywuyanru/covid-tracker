4ad1e9c3e0fb84d67967365689b6fe721f0a5c26
Lily <lilywuyanru@gmail.com>
2020-04-12 03:40:16 -0400

    Add routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import React from 'react';
 import './App.css';
 import { Navigation } from './components/Navigation/Navigation.js';
 import { SearchSelection } from './components/SearchSelection/SearchSelection.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-        <Navigation />
-        <br />
-        <div class="content" align="center">
-          <SearchSelection />
-        </div>
-        <br />
+        <Router>
+          <Navigation />
+          <br />
+          <Switch>
+            <Route exact path="/">
+              <div class="content" align="center">
+                <SearchSelection />
+              </div>
+            </Route>
+            <Route exact path="/about">
+              <div class="content" align="center">
+                <h1>just tryna figure out routes</h1>
+              </div>
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
